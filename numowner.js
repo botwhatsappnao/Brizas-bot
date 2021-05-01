6c337bdae524eea71124143f4b1bca87929f80fc
botwhatsappnao <80915720+botwhatsappnao@users.noreply.github.com>
2021-05-01 17:43:06 -0300

    Update numowner.js
    A

--- numowner.js
+++ numowner.js
@@ -1,5 +1,5 @@
-const ownerNumber = ["14502355564@s.whatsapp.net"]
-const OriginalOwner = '14502355564'
+Contacto Creador = ["5491144294789@s.whatsapp.net"]
+Contacto Real del Creador = '5491144294789'
 
 exports.OriginalOwner = OriginalOwner
 exports.ownerNumber = ownerNumber
